d940409ce913bfaade63a7480bd74732f59545dc
Ale Olivati <ale.olivati@gmail.com>
2023-06-01 12:55:14 -0300

    add style in sidebar

--- src/components/Paragraph/index.tsx
+++ src/components/Paragraph/index.tsx
@@ -3,10 +3,13 @@ import { Paragraph } from './styles'
 export type Props = {
   children: string
   type?: 'main' | 'secondary'
+  fontSize?: number
 }
 
-const P = ({ children, type = 'main' }: Props) => (
-  <Paragraph type={type}>{children}</Paragraph>
+const P = ({ children, type = 'main', fontSize }: Props) => (
+  <Paragraph type={type} fontSize={fontSize}>
+    {children}
+  </Paragraph>
 )
 
 export default P
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,19 @@
+import Avatar from '../../components/Avatar'
+import P from '../../components/Paragraph'
 import Title from '../../components/Title'
+import { Description, SidebarContainer, ThemeButton } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Aleolivati.png" />
-    <Title fontSize={20}>Alexandre Olivati Sabatini Spindola</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={20}>Alexandre Olivati Sabatini Spindola</Title>
+      <P type="secondary" fontSize={16}>
+        Aleolivati
+      </P>
+      <Description type="main"> Engenheiro front-end </Description>
+      <ThemeButton>Troca tema</ThemeButton>
+    </SidebarContainer>
   </aside>
 )
 
