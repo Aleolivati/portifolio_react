12855e912d6c7ce330d46234acfd6c44e0a7e714
Ale Olivati <ale.olivati@gmail.com>
2023-06-01 11:57:55 -0300

    add projects section

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,13 @@
+import P from '../Paragraph'
+import Title from '../Title'
+import { Card, LinkButton } from './styles'
+
+const Project = () => (
+  <Card>
+    <Title> TESTE PROJETO </Title>
+    <P> Teste teste teste</P>
+    <LinkButton>Visualizar</LinkButton>
+  </Card>
+)
+
+export default Project
